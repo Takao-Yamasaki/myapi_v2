6c8f19a8ac8af393aeadbd873b18b1c0fde853fc
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2023-01-23 22:53:47 +0900

    ポインタ

--- main.go
+++ main.go
@@ -16,8 +16,22 @@ func (p *Person) Greeting() {
 	fmt.Printf("Hello, my name is %s\n", p.name)
 }
 
+// ポインタを使わずPersonを受け取る
+func (p Person) addHeight() {
+	p.height += 10
+}
+
+// ポインタを使ってPersonを受け取る
+func (p *Person) addWeight() {
+	p.weight += 10
+}
+
 func main() {
-	p := Person{name: "hsaki"}
-	// p: レシーバー
-	p.Greeting()
+	q := Person{height: 160, weight: 50}
+	fmt.Println(q) // {160 50 }
+
+	q.addHeight()
+	q.addWeight()
+
+	fmt.Println(q) // {160 60 }
 }
