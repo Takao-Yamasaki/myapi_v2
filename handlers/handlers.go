2b5834944ffdbd0573fca5de4b83fd28e65955ff
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2023-01-26 20:47:53 +0900

    リクエストヘッダの長さを取得

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -19,16 +20,21 @@ func HelloHandler(w http.ResponseWriter, req *http.Request) {
 // /articleのハンドラ
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
 	// TODO: バイトスライスをなんらかの形で用意
-	var reqBodybuffer []byte
+	length, err := strconv.Atoi(req.Header.Get("Content-Length"))
+	if err != nil {
+		http.Error(w, "cannot get content length\n", http.StatusBadRequest)
+		return
+	}
+	reqBodybuffer := make([]byte, length)
 
 	// リクエストボディの読み出し
-	if _, err := req.Body.Read(reqBodybuffer); !errors.ls(err, io.EOF) {
+	if _, err := req.Body.Read(reqBodybuffer); !errors.Is(err, io.EOF) {
 		http.Error(w, "fail to get request body\n", http.StatusBadRequest)
 		return
 	}
 
 	defer req.Body.Close()
-	
+
 	article := models.Article1
 	jsonData, err := json.Marshal(article)
 	if err != nil {
