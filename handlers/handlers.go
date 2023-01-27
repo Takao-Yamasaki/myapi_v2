3dc20ab754914a2a7467247f0542cdb5b505bccb
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2023-01-27 09:31:50 +0900

    decoderの導入

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -19,26 +19,11 @@ func HelloHandler(w http.ResponseWriter, req *http.Request) {
 
 // /articleのハンドラ
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
-	// TODO: バイトスライスをなんらかの形で用意
-	length, err := strconv.Atoi(req.Header.Get("Content-Length"))
-	if err != nil {
-		http.Error(w, "cannot get content length\n", http.StatusBadRequest)
-		return
-	}
-	reqBodybuffer := make([]byte, length)
-
-	// リクエストボディの読み出し
-	if _, err := req.Body.Read(reqBodybuffer); !errors.Is(err, io.EOF) {
-		http.Error(w, "fail to get request body\n", http.StatusBadRequest)
-		return
-	}
-
-	defer req.Body.Close()
-
 	var reqArticle models.Article
-	if err := json.Unmarshal(reqBodybuffer, &reqArticle); err != nil {
+
+	// デコーダの導入
+	if err := json.NewDecoder(req.Body).Decode(&reqArticle); err != nil {
 		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
-		return
 	}
 
 	article := reqArticle
