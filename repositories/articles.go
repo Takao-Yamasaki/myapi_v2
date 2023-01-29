27e7c974fd8eff7ef8e2e835f95f8187a11de6c1
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2023-01-29 22:04:00 +0900

    repositoriesの実装

---
+++ repositories/articles.go
@@ -0,0 +1,121 @@
+package repositories
+
+import (
+	"database/sql"
+
+	"github.com/Takao-Yamasaki/myapi_v2/models"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func InsertArticle(db *sql.DB, article models.Article) (models.Article, error) {
+	const sqlStr = `
+		insert into articles (title, contents, username, nice, created_at)
+		values (?, ?, ?, 0, now());
+	`
+	var newArticle models.Article
+	newArticle.Title, newArticle.Contents, newArticle.UserName, newArticle.NiceNum, newArticle.CreatedAt = article.Title, article.Contents, article.UserName, article.NiceNum, article.CreatedAt
+
+	result, err := db.Exec(sqlStr, article.Title, article.Contents, article.UserName, article.NiceNum, article.CreatedAt)
+	if err != nil {
+		return models.Article{}, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return models.Article{}, err
+	}
+
+	newArticle.ID = int(id)
+
+	return newArticle, nil
+}
+
+func SelectArticleList(db *sql.DB, page int) ([]models.Article, error) {
+	const sqlStr = `
+		select article_id, title, contents, username, nice
+		from articles
+		limit ? offset ?
+	`
+	articleArray := make([]models.Article, 0)
+
+	rows, err := db.Query(sqlStr, 5, page)
+	if err != nil {
+		return []models.Article{}, err
+	}
+
+	var article models.Article
+	for rows.Next() {
+		err = rows.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.NiceNum)
+		if err != nil {
+			return []models.Article{}, err
+		}
+		articleArray = append(articleArray, article)
+	}
+	return articleArray, nil
+}
+
+func SelectArticleDatail(db *sql.DB, articleID int) (models.Article, error) {
+	const sqlStr = `
+		select *
+		from articles
+		where article_id = ?;
+	`
+
+	var article models.Article
+	var createdTime sql.NullTime
+
+	row := db.QueryRow(sqlStr, articleID)
+	if err := row.Err(); err != nil {
+		return models.Article{}, err
+	}
+
+	err := row.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.NiceNum, &article.CommentList, &createdTime)
+	if err != nil {
+		return models.Article{}, err
+	}
+
+	if createdTime.Valid {
+		article.CreatedAt = createdTime.Time
+	}
+
+	return article, nil
+}
+
+func UpdateNiceNum(db *sql.DB, articleID int) error {
+	const sqlGetNice = `
+		select nice
+		from articles
+		where article_id = ?;
+	`
+
+	const sqlUpdateNice = `update articles set nice = ? where article_id = ?`
+
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+
+	row := tx.QueryRow(sqlGetNice, articleID)
+	if err := row.Err(); err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	var article models.Article
+	if err = row.Scan(&article.NiceNum); err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	_, err = tx.Exec(sqlUpdateNice, article.NiceNum+1, articleID)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	if err = tx.Commit(); err != nil {
+		tx.Rollback()
+		return err
+	}
+	return nil
+}
---
+++ repositories/comments.go
@@ -0,0 +1,58 @@
+package repositories
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/Takao-Yamasaki/myapi_v2/models"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func InsertComment(db *sql.DB, comment models.Comment) (models.Comment, error) {
+	const sqlStr = `
+		insert into comments (article_id, message, created_at) values
+		(?, ?, now());
+	`
+	var newComment models.Comment
+	newComment.ArticleID, newComment.Message, newComment.CreatedAt = comment.ArticleID, comment.Message, comment.CreatedAt
+	result, err := db.Exec(sqlStr, comment.ArticleID, comment.Message, comment.CreatedAt)
+	if err != nil {
+		return models.Comment{}, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return models.Comment{}, err
+	}
+
+	newComment.CommentID = int(id)
+	return newComment, nil
+}
+
+func SelectCommentList(db *sql.DB, articleID int) ([]models.Comment, error) {
+	const sqlStr = `
+		select *
+		from comments
+		where article_id = ?;
+	`
+
+	rows, err := db.Query(sqlStr, articleID)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer rows.Close()
+
+	commentArray := make([]models.Comment, 0)
+
+	for rows.Next() {
+		var createdTime sql.NullTime
+		var comment models.Comment
+
+		rows.Scan(&comment.CommentID, &comment.ArticleID, &comment.Message, &createdTime)
+		if createdTime.Valid {
+			comment.CreatedAt = createdTime.Time
+		}
+		commentArray = append(commentArray, comment)
+	}
+	return commentArray, nil
+}
