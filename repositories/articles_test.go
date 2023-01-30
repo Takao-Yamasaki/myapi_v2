2d823ab7228a3670c89245a0daafaf6c3c5eddda
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2023-01-30 09:15:43 +0900

    テーブルドリブンテストの実装

--- repositories/articles_test.go
+++ repositories/articles_test.go
@@ -11,7 +11,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func TestSelectArticleDetail1(t *testing.T) {
+func TestSelectArticleDetail(t *testing.T) {
 	dbUser := "docker"
 	dbPassword := "docker"
 	dbDatabase := "sampledb"
@@ -25,66 +25,57 @@ func TestSelectArticleDetail1(t *testing.T) {
 	defer db.Close()
 
 	// テスト結果で期待する値の定義
-	expected := models.Article{
-		ID:       1,
-		Title:    "firstPost",
-		Contents: "This is my first blog",
-		UserName: "saki",
-		NiceNum:  3,
+	tests := []struct {
+		testTitle string
+		expected  models.Article
+	}{
+		{
+			testTitle: "subtest1",
+			expected: models.Article{
+				ID:       1,
+				Title:    "firstPost",
+				Contents: "This is my first blog",
+				UserName: "saki",
+				NiceNum:  3,
+			},
+		}, {
+			testTitle: "subtest2",
+			expected: models.Article{
+				ID:       2,
+				Title:    "2nd",
+				Contents: "Second blog post",
+				UserName: "saki",
+				NiceNum:  4,
+			},
+		},
 	}
 
-	// テスト対象となる関数の実行
-	got, err := repositories.SelectArticleDatail(db, expected.ID)
-	if err != nil {
-		// エラーであれば、そもそもテストを実行することができないので、fatalでテストを終了させる
-		t.Fatal(err)
+	for _, test := range tests {
+		t.Run(test.testTitle, func(t *testing.T) {
+			// テスト対象となる関数の実行
+			got, err := repositories.SelectArticleDatail(db, test.expected.ID)
+			if err != nil {
+				// エラーであれば、そもそもテストを実行することができないので、fatalでテストを終了させる
+				t.Fatal(err)
+			}
+
+			// 期待する出力と実際の出力が一致するか比較する
+			if got.ID != test.expected.ID {
+				t.Errorf("ID: get %d but want %d\n", got.ID, test.expected.ID)
+			}
+			if got.Title != test.expected.Title {
+				t.Errorf("Title: get %s but want %s", got.Title, test.expected.Title)
+			}
+			if got.Contents != test.expected.Contents {
+				t.Errorf("Contents: get %s but want %s", got.Contents, test.expected.Contents)
+			}
+			if got.UserName != test.expected.UserName {
+				t.Errorf("UserName: get %s but want %s\n", got.UserName, test.expected.UserName)
+			}
+			if got.NiceNum != test.expected.NiceNum {
+				t.Errorf("NiceNum: get %d but want %d\n", got.NiceNum, test.expected.NiceNum)
+			}
+		})
 	}
 
-	// 期待する出力と実際の出力が一致するか比較する
-	if got.ID != expected.ID {
-		t.Errorf("ID: get %d but want %d\n", got.ID, expected.ID)
-	}
-	if got.Title != expected.Title {
-		t.Errorf("Title: get %s but want %s", got.Title, expected.Title)
-	}
-	if got.Contents != expected.Contents {
-		t.Errorf("Contents: get %s but want %s", got.Contents, expected.Contents)
-	}
-	if got.UserName != expected.UserName {
-		t.Errorf("UserName: get %s but want %s\n", got.UserName, expected.UserName)
-	}
-	if got.NiceNum != expected.NiceNum {
-		t.Errorf("NiceNum: get %d but want %d\n", got.NiceNum, expected.NiceNum)
-	}
-}
-
-func TestSelectArticleDetail2(t *testing.T) {
-	dbUser := "docker"
-	dbPassword := "docker"
-	dbDatabase := "sampledb"
-	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
-
-	// データベースへ接続
-	db, err := sql.Open("mysql", dbConn)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer db.Close()
-
-	expected := models.Article{
-		ID:       2,
-		Title:    "2nd",
-		Contents: "Second blog post",
-		UserName: "saki",
-		NiceNum:  4,
-	}
-
-	got, err := repositories.SelectArticleDatail(db, expected.ID)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if got.ID != expected.ID {
-		t.Errorf("ID: get %d but want %d\n", got.ID, expected.ID)
-	}
 }
