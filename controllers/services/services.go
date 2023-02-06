22c3ccea985397428b33ac210fdebb3debd06dda
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2023-02-07 07:46:27 +0900

    サービスインターフェースの分離

--- controllers/services/services.go
+++ controllers/services/services.go
@@ -4,11 +4,15 @@ package services
 
 import "github.com/Takao-Yamasaki/myapi_v2/models"
 
-type MyAppServicer interface {
+// article関連を引き受けるサービス
+type ArticleServicer interface {
 	PostArticleService(article models.Article) (models.Article, error)
 	GetArticleListService(page int) ([]models.Article, error)
 	GetArticleService(articleID int) (models.Article, error)
 	PostNiceService(article models.Article) (models.Article, error)
+}
 
+// comment関連を引き受けるサービス
+type CommentServicer interface {
 	PostCommentService(comment models.Comment) (models.Article, error)
 }
