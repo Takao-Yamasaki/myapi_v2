228578127cb25fbb151de6a7ed3f54cb3ef9de61
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2023-02-25 16:40:59 +0900

    remove:不要なコードを削除した

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 
 	"github.com/Takao-Yamasaki/myapi_v2/api"
 	_ "github.com/go-sql-driver/mysql"
@@ -20,26 +19,6 @@ var (
 )
 
 func main() {
-	_, err1 := strconv.Atoi("a")
-
-	err2 := strconv.ErrSyntax
-	err3 := strconv.ErrRange
-
-	fmt.Println(err1 == err2)
-	fmt.Println(err1 == err3)
-
-	// fmt.Println(errors.Is(err1, err2))
-	// fmt.Println(errors.Is(err1, err3))
-	// aは数値に直せないので、エラーが発生する
-	// _, err0 := strconv.Atoi("a")
-	// fmt.Printf("err0: [%T] %v\n", err0, err0)
-
-	// err1 := errors.Unwrap(err0)
-	// fmt.Printf("err1: [%T] %v\n", err1, err1)
-
-	// err2 := errors.Unwrap(err1)
-	// fmt.Printf("err2: [%T] %v\n", err2, err2)
-
 	db, err := sql.Open("mysql", dbConn)
 	if err != nil {
 		log.Println("fail to connect DB")
