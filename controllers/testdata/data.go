780dec272fb9d1c3894aec7a448ecd3efb27357f
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2023-02-25 21:21:26 +0900

    fix:整形した

--- controllers/testdata/data.go
+++ controllers/testdata/data.go
@@ -2,33 +2,33 @@ package testdata
 
 import "github.com/Takao-Yamasaki/myapi_v2/models"
 
-var articleTestData = []models.Article {
+var articleTestData = []models.Article{
 	models.Article{
-		ID: 1,
-		Title: "firstPost",
-		Contents: "This is my first blog",
-		UserName: "saki",
-		NiceNum: 2,
+		ID:          1,
+		Title:       "firstPost",
+		Contents:    "This is my first blog",
+		UserName:    "saki",
+		NiceNum:     2,
 		CommentList: commentTestData,
 	},
 	models.Article{
-		ID: 2,
-		Title: "2nd",
+		ID:       2,
+		Title:    "2nd",
 		Contents: "Second blog post",
-		UseName: "saki",
-		NiceNum: 4,
+		UserName: "saki",
+		NiceNum:  4,
 	},
 }
 
-var commentTestData = []models.Comment {
+var commentTestData = []models.Comment{
 	models.Comment{
 		CommentID: 1,
 		ArticleID: 1,
-		Message: "1st comment yeah",
+		Message:   "1st comment yeah",
 	},
 	models.Comment{
 		CommentID: 2,
 		ArticleID: 1,
-		Message: "welcome",
-	}
+		Message:   "welcome",
+	},
 }
